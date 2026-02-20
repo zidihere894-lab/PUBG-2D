3bcc6d2230cccf4f16ab5e61d505b85fdb038c0c
Ziddihacks086 <zidihere894@gmail.com>
2026-02-21 00:52:36 +0500

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,34 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCFy28nNtxhkjfpOd0aCfYkjHUwErh1WVQ",
+    authDomain: "movie-1e6fc.firebaseapp.com",
+    projectId: "movie-1e6fc",
+    storageBucket: "movie-1e6fc.firebasestorage.app",
+    messagingSenderId: "371616954594",
+    appId: "1:371616954594:web:977d88ec7570df351246bb"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize services
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+// Enable offline persistence
+db.enablePersistence()
+    .catch((err) => {
+        if (err.code == 'failed-precondition') {
+            console.log('Multiple tabs open, persistence disabled');
+        } else if (err.code == 'unimplemented') {
+            console.log('Browser doesn\'t support persistence');
+        }
+    });
+
+// Export for use in other files
+window.firebaseApp = {
+    auth: auth,
+    db: db,
+    storage: storage
+};
